6ba473c8d4ba13807e30911358749281436c53c5
YASUI Kentarow <yasuikentarow@gmail.com>
2021-11-04 21:32:14 +0900

    glmもwxEditor以下を使うように

--- external/build/mac/setting.mk
+++
@@ -1,9 +0,0 @@
-DEPLOYMENT_TARGET	:= 11.0
-
-CXX		:= clang++
-CXXFLAGS	:= -std=c++11 -stdlib=libc++
-
-CURDIR		:= $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
-TOPDIR		:= $(abspath $(CURDIR)/../../..)
-WORKDIR		:= $(CURDIR)/work
-OPTDIR		:= $(TOPDIR)/external/opt/mac
